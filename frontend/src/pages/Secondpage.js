156e7f8995f87d7df07068c77857c525f0f5c89e
rahulMHC <rarya2552@gmail.com>
2023-04-22 13:59:31 +0530

    Secondpage having ide

--- frontend/src/pages/Secondpage.js
+++ frontend/src/pages/Secondpage.js
@@ -1,7 +1,92 @@
+import { useState } from "react";
+import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
+import Textarea from "@mui/joy/Textarea";
+import axios from "axios";
+import NavBar from "./Navbar";
+import { Grid } from "@mui/material";
+
 export default function Secondpage() {
+  const [inputStr, setcode] = useState(``);
+
+  const handleSubmit = () => {
+    const codeStr = inputStr
+      .replace(/\n/g, "\\n")
+      .replace(/\t/g, "\\t")
+      .replace(/;/g, "");
+
+    console.log(codeStr);
+    // TODO Send the input as hidden tests and recieve the output by output api
+    // Language selection dropdown
+    axios
+      .post(
+        "https://codejudge.geeksforgeeks.org/submit-request",
+        {
+          language: "c",
+          code: codeStr,
+          input: "",
+          save: false,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response.data[0]);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
-    <>
-      <h1>Second</h1>
-    </>
+    <div style={{ backgroundColor: "#498EC5", width: "100vw", height: "100%" }}>
+      <NavBar />
+      <Grid container columns={{ xs: 2, md: 12 }}>
+        <Grid item xs={6} style={{ backgroundColor: "black" }}>
+          <p style={{ color: "white", fontSize: "20px", margin: "10%" }}>
+            <h2 style={{ margin: "5px" }}>
+              Minimum Insertion Steps to Make a String Palindrome
+            </h2>
+            Given a string s. In one step you can insert any character at any
+            index of the string. Return the minimum number of steps to make s
+            palindrome. A Palindrome String is one that reads the same backward
+            as well as forward.
+          </p>
+        </Grid>
+        <Grid item xs={6} style={{ padding: "5px" }}>
+          <Textarea
+            placeholder="Write your code Here…"
+            minRows={25}
+            endDecorator={
+              <Box
+                sx={{
+                  display: "flex",
+                  gap: "var(--Textarea-paddingBlock)",
+                  pt: "var(--Textarea-paddingBlock)",
+                  borderTop: "1px solid",
+                  borderColor: "divider",
+                  flex: "auto",
+                }}
+              >
+                <Button onClick={handleSubmit} sx={{ ml: "auto" }}>
+                  Submit
+                </Button>
+              </Box>
+            }
+            sx={{
+              minWidth: 300,
+            }}
+            style={{ backgroundColor: "#000030", color: "white" }}
+            onChange={(e) => {
+              setcode(e.target.value);
+            }}
+          />
+        </Grid>
+      </Grid>
+    </div>
   );
 }
